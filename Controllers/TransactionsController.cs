c5419330811f62ec8b71ff5f9475afdbf5a1891a
romflz <electronicmusicplease@gmail.com>
2026-04-22 15:54:38 +0200

    feat: pagination of transactions

--- Controllers/TransactionsController.cs
+++ Controllers/TransactionsController.cs
@@ -17,15 +17,27 @@ public class TransactionsController : Controller
     }
 
     [HttpGet("/transactions")]
-    public async Task<IActionResult> Index()
+    public async Task<IActionResult> Index(int page = 1)
     {
+        const int pageSize = 10;
+        if (page < 1) page = 1;
+
+        var totalCount = await _context.Transactions.CountAsync();
+        var totalPages = Math.Max(1, (int)Math.Ceiling(totalCount / (double)pageSize));
+        if (page > totalPages) page = totalPages;
+
         var transactions = await _context.Transactions
             .OrderByDescending(t => t.CreatedAt)
+            .Skip((page - 1) * pageSize)
+            .Take(pageSize)
             .ToListAsync();
 
         // Decrypt the IBAN for display
         foreach (var t in transactions)
             t.EncryptedIban = _encryptionService.Decrypt(t.EncryptedIban);
+
+        ViewData["CurrentPage"] = page;
+        ViewData["TotalPages"] = totalPages;
 
         return View(transactions);
     }
